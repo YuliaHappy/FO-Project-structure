561ff7d89295bdb0aab81b44c224fa4c19eeb6b4
Iuliia Savich <Iuliia_Savich@epam.com>
2016-06-10 11:34:26 +0300

    fix bugs

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,33 @@ const gulp = require('gulp');
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 const gulpIf = require('gulp-if');
-const rename = require('gulp-rename'); 
 const sourcemaps = require('gulp-sourcemaps');
 const gutil = require('gulp-util');
 
-//Склейка в один бандл
+//Склейка в один бандл, 
+//процессинг JS (BabelJs), 
+//минификация и создание map-файла
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 gulp.task('browserify', function() {
-  return browserify(['frontend/scripts/index.js', 'frontend/scripts/subIndex.js'])
+  var files = require('glob').sync("frontend/scripts/index.js");
+  return browserify({
+      enteries: ['frontend/scripts/index.js'],
+      debug: true
+    })
+    .transform(require('babelify'))
     .bundle()
+    .on('error', gutil.log)
     .pipe(source('bundle.js'))
+    .pipe(require('vinyl-buffer')())
+    .pipe(require('gulp-uglify')())
     .pipe(gulp.dest('public/scripts/browserify'));
 });
 
 //Unit-тестирование
 gulp.task('test', function() {
-  return gulp.src(['frontend/test/**/*.spec.js'], {read: false})
+  return gulp
+    .src(['frontend/test/**/*.spec.js'], {read: false})
     .pipe(require('gulp-mocha')({ reporter: 'spec' }))
     .on('error', gutil.log);
 ;
@@ -32,26 +42,14 @@ gulp.task('clean', function() {
     return require('del')('public');
 });
 
-//Процессинг JS (BabelJs), минификация и создание map-файла
-gulp.task('babel', () =>
-	gulp.src('frontend/scripts/**/*.js')
-		.pipe(gulpIf(isDevelopment, sourcemaps.init()))
-		.pipe(require('gulp-babel')())
-        .pipe(gulp.dest('public/scripts/babel'))
-        .pipe(require('gulp-uglify')())
-        .pipe(rename('scripts.min.js'))
-		.pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
-		.pipe(gulp.dest('public/scripts/minify'))
-);
-
 //Процессинг css
 const debug = require('gulp-debug');
 gulp.task('styles', function() {
-	return gulp.src('frontend/styles/main.styl', {base: 'frontend'})
+	return gulp
+    .src('frontend/styles/main.styl', {base: 'frontend'})
 		.pipe(gulpIf(isDevelopment, sourcemaps.init()))
-		.pipe(debug({title: 'after src'}))
 		.pipe(require('gulp-stylus')())
-		.pipe(debug({title: 'after stylus'}))
+    .pipe(require('gulp-concat')('style.css'))
 		.pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
 		.pipe(gulp.dest('public'));
 });
@@ -67,17 +65,6 @@ gulp.task('autoprefixer', function() {
         .pipe(gulp.dest('public'));
 });
 
-
-gulp.task('assets', function() {
-	return gulp.src('frontend/**/*.html')
-		.pipe(gulp.dest('public'));
-});
-
-gulp.task('build', gulp.series(
-	'clean',	
-	gulp.parallel('babel', 'styles', 'autoprefixer', 'assets'))
-);
-
 //Инкрементальная сборка
 gulp.task('watch', function() {
   gulp.watch('frontend/scripts/**/*.js', 
@@ -88,8 +75,40 @@ gulp.task('watch', function() {
     gulp.series('styles'));
   gulp.watch('frontend/styles/**/*.css',
     gulp.series('autoprefixer'));
-  gulp.watch('frontend//**/*.html',
+  gulp.watch('frontend/**/*.html',
     gulp.series('assets'));
 });
 
-gulp.task('default', gulp.series('test', 'build', 'browserify', 'watch'));
+gulp.task('assets', function() {
+    return gulp
+      .src('frontend/**/*.html', {
+            since: gulp.lastRun('assets')
+        })
+        .pipe(debug({
+            title: 'assets'
+        }))
+        .pipe(gulp.dest('public'));
+});
+
+const browserSync = require('browser-sync').create();
+gulp.task('serve', function() {
+    browserSync.init({
+        server: 'public'
+    });
+    browserSync.watch('public/**/*.*')
+      .on('change', browserSync.reload);
+});
+
+gulp.task('build', gulp.series(
+  'clean',  
+  gulp.parallel('styles', 'autoprefixer'))
+);
+
+gulp.task('default', gulp.series('test', 'build', 'browserify'));
+
+//Запуск странички в браузере 
+//+ мониторинг и изменения в реальном времени
+gulp.task('dev',
+    gulp.series(
+      gulp.series('default', 'assets'), 
+      gulp.parallel('watch', 'serve')));
